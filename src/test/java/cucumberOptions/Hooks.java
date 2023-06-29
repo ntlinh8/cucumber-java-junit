fb3fab1bd4157462b35203a45c608e11d015be19
Nguyen Thuy Linh <thuylinh2409nguyen@gmail.com>
2023-06-29 17:43:54 +0700

    Upload Hooks file using @After and @Before

---
+++ src/test/java/cucumberOptions/Hooks.java
@@ -0,0 +1,100 @@
+package cucumberOptions;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.UnreachableBrowserException;
+
+import commons.GlobalConstants;
+import cucumber.api.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Hooks {
+	private static WebDriver driver;
+	private static final Logger log = Logger.getLogger(Hooks.class.getName());
+
+	@Before
+	public synchronized static WebDriver openAndQuitBrowser() {
+		String browser = System.getProperty("BROWSER");
+		System.out.println("Browser name run by command line = " + browser);
+
+		if (driver == null) {
+			try {
+				switch (browser) {
+				case "chrome":
+					WebDriverManager.chromedriver().setup();
+					driver = new ChromeDriver();
+					break;
+				case "hchrome":
+					WebDriverManager.chromedriver().setup();
+					ChromeOptions chromeOptions = new ChromeOptions();
+					chromeOptions.addArguments("headless");
+					chromeOptions.addArguments("window-size=1920x1080");
+					driver = new ChromeDriver(chromeOptions);
+					break;
+				case "firefox":
+					WebDriverManager.firefoxdriver().setup();
+					System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+					System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
+					driver = new FirefoxDriver();
+					break;
+				case "hfirefox":
+					WebDriverManager.firefoxdriver().setup();
+					System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+					System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
+					FirefoxOptions firefoxOptions = new FirefoxOptions();
+					firefoxOptions.setHeadless(true);
+					driver = new FirefoxDriver(firefoxOptions);
+					break;
+				case "ie":
+					WebDriverManager.iedriver().arch32().setup();
+					driver = new InternetExplorerDriver();
+					break;
+				default:
+					WebDriverManager.chromedriver().browserVersion("78.0.3904.70").setup();
+					driver = new ChromeDriver();
+					break;
+				}
+			} catch (UnreachableBrowserException e) {
+				driver = new ChromeDriver();
+			} catch (WebDriverException e) {
+				driver = new ChromeDriver();
+			}
+			finally {
+				Runtime.getRuntime().addShutdownHook(new Thread(new BrowserCleanup()));
+			}
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+			driver.get(GlobalConstants.BANK_GURU_REGISTER_URL);
+			log.info("------------- Started the browser -------------");
+			
+		}
+		return driver;
+	}
+
+	public static void close() {
+		try {
+			if (driver != null) {
+				openAndQuitBrowser().quit();
+				log.info("------------- Closed the browser -------------");
+			}
+		} catch (UnreachableBrowserException e) {
+			System.out.println("Can not close the browser");
+		}
+	}
+
+	private static class BrowserCleanup implements Runnable {
+		@Override
+		public void run() {
+			close();
+		}
+	}
+
+}
